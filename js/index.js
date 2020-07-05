6093a124a8a91ff1208aa1bfc5c74c14a7fd9814
Nicole Tilbe <nicolecali2406@outlook.com>
2020-07-05 16:01:11 -0400

    fixed JS task list

--- js/index.js
+++ js/index.js
@@ -44,8 +44,7 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 // ## Task 1: Create selectors to point your data into elements
 
 // * [ ] Create selectors by using any of the DOM element's methods
-// * [ ] Note that IDs have been used on all images.  Use the IDs to update src path content
-
+// * [ ] Note that IDs have been used on all images. Use the IDs to update src path content
 
 
 
@@ -55,13 +54,19 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 // * [ ] Using your selectors, update the content to match the example file.
 // * [ ] Remember to update the src attributes on images
 
+
+
 // ## Task 3: Add new content
 
-//Check your work by looking at the [original html](original.html) in the browser
+// * [ ] Change the color of the navigation text to be green.
+// * [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
+// * [ ] Check your work by looking at the [original html](original.html) in the browser
+
+
 
 // ## Stretch Goals
 
-// * [ ] Update styles throughout the page as you see fit.  Study what happens when you updated the DOM using style in JavaScript.  
+// * [ ] Update styles throughout the page as you see fit. Study what happens when you updated the DOM using style in JavaScript.  
 // * [ ] Study tomorrow's lesson on events and try to integrate a button that can update content on the site with a click of a button.  You could build a similar data object with new values to help you test the click event.
 
 // ## Stretch Project: Digital Timer
